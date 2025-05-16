10f6a0bbbaad474271df330addc797607437b537
MarinaDP2006 <marinadompas@gmail.com>
2025-05-16 13:52:10 +0200

    Update Reparto.java

--- Programacion/src/dto/Reparto.java
+++ Programacion/src/dto/Reparto.java
@@ -1,52 +1,52 @@
 package dto;
 
+/**
+ * Clase que representa el reparto de una película.
+ */
 public class Reparto {
-	private int IDpelicula;
-	private int IDactor;
-	private String personaje;
-	
-    /** Constructor para crear un nuevo reparto.
-    * @param personaje 
-    * @param actor
-    * @param personaje
-    */
-	public Reparto(int IDpelicula, int IDactor, String personaje) {
-		super();
-		this.IDpelicula = IDpelicula;
-		this.IDactor = IDactor;
-		this.personaje = personaje;
-	}
+    private int IDpelicula;
+    private int IDactor;
+    private String personaje;
 
-// GET Y SET
-	public int getPelicula() {
-		return IDpelicula;
-	}
+    /**
+     * Constructor para crear un nuevo reparto.
+     * @param IDpelicula Identificador de la película
+     * @param IDactor Identificador del actor
+     * @param personaje Nombre del personaje interpretado por el actor
+     */
+    public Reparto(int IDpelicula, int IDactor, String personaje) {
+        this.IDpelicula = IDpelicula;
+        this.IDactor = IDactor;
+        this.personaje = personaje;
+    }
 
+    // GET Y SET
+    public int getPelicula() {
+        return IDpelicula;
+    }
 
-	public void setPelicula(int pelicula) {
-		this.IDpelicula = pelicula;
-	}
+    public void setPelicula(int pelicula) {
+        this.IDpelicula = pelicula;
+    }
 
-	public int getActor() {
-		return IDactor;
-	}
+    public int getActor() {
+        return IDactor;
+    }
 
-	public void setActor(int actor) {
-		this.IDactor = actor;
-	}
+    public void setActor(int actor) {
+        this.IDactor = actor;
+    }
 
-	public String getPersonaje() {
-		return personaje;
-	}
+    public String getPersonaje() {
+        return personaje;
+    }
 
-	public void setPersonaje(String personaje) {
-		this.personaje = personaje;
-	}
-	
-	 /** Aparecen las peliculas actores y personajes que intervienen en ella.
-     * @return Lista con toda la informacion */
-	
-	public String toString() {
-		return "Pelicula: " + IDpelicula + "\n" + "Actor: " + IDactor + "\n" + "Personaje: " + personaje;
-	}
+    public void setPersonaje(String personaje) {
+        this.personaje = personaje;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula: " + IDpelicula + "\n" + "Actor: " + IDactor + "\n" + "Personaje: " + personaje;
+    }
 }
